3bf1d6fd31699bdf8fd333aa61c013c3ea3219aa
p-mazhnik <pavel.mazhnik@gmail.com>
2021-09-25 21:43:56 +0300

    ios: implement startFlutterActivity function

--- flutter_module_rn/ios-rn/FlutterModuleRn.h
+++ flutter_module_rn/ios-rn/FlutterModuleRn.h
@@ -1,7 +1,10 @@
 // FlutterModuleRn.h
 
 #import <React/RCTBridgeModule.h>
+@import Flutter;
 
 @interface FlutterModuleRn : NSObject <RCTBridgeModule>
+
++ (void)initWithFlutterEngine:(FlutterEngine * _Nonnull)flutterEngine;
 
 @end
--- flutter_module_rn/ios-rn/FlutterModuleRn.m
+++ flutter_module_rn/ios-rn/FlutterModuleRn.m
@@ -1,16 +1,38 @@
 // FlutterModuleRn.m
 
+@import Flutter;
+@import UIKit;
 #import "FlutterModuleRn.h"
 
+static FlutterEngine *_flutterEngine = nil;
 
 @implementation FlutterModuleRn
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
++ (void)initWithFlutterEngine:(FlutterEngine * _Nonnull)flutterEngine {
+    _flutterEngine = flutterEngine;
+}
+
+RCT_EXPORT_METHOD(startFlutterActivity:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        // init FlutterViewController with engine provided by host app
+        FlutterViewController *flutterViewController;
+         if (_flutterEngine == nil) {
+            // It is not recommended, but we can create a FlutterViewController with an implicit FlutterEngine
+            // https://flutter.dev/docs/development/add-to-app/ios/add-flutter-screen?tab=engine-objective-c-tab#alternatively---create-a-flutterviewcontroller-with-an-implicit-flutterengine
+            flutterViewController = [[FlutterViewController alloc] initWithProject:nil nibName:nil bundle:nil];
+         } else {
+            flutterViewController = [[FlutterViewController alloc] initWithEngine:_flutterEngine nibName:nil bundle:nil];
+         }
+        // fix ui
+        [flutterViewController setModalPresentationStyle:UIModalPresentationFullScreen];
+
+        UIViewController *rootController = UIApplication.sharedApplication.delegate.window.rootViewController;
+        [rootController presentViewController:flutterViewController animated:YES completion:nil];
+        callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    });
 }
 
 @end
