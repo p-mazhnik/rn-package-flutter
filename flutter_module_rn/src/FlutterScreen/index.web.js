22dcdf97b92e6aacf41775f0a3540c1a742ca260
p-mazhnik <pavel.mazhnik@gmail.com>
2023-07-16 12:46:11 +0300

    refactor(rn/web): remove useEffect from the FlutterScreen

--- flutter_module_rn/src/FlutterScreen/index.web.js
+++ flutter_module_rn/src/FlutterScreen/index.web.js
@@ -1,19 +1,21 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 
 const FlutterScreen = () => {
-  const iframeRef = useRef(null);
-  useEffect(() => {
-    const iframe = iframeRef.current;
-    iframe.setAttribute('referrerpolicy', 'no-referrer')
-    iframe.setAttribute('sandbox', 'allow-scripts allow-same-origin')
-  }, [])
-
-  return <iframe src='/flutter' ref={iframeRef} style={{
-    height: '100vh',
-    width: '100vw',
-    top: 0,
-    border: 0,
-  }} />
+  const iframeRef = useRef(null)
+  return (
+    <iframe
+      src="/flutter"
+      ref={iframeRef}
+      style={{
+        height: '100vh',
+        width: '100vw',
+        top: 0,
+        border: 0,
+      }}
+      sandbox='allow-scripts allow-same-origin'
+      referrerPolicy='no-referrer'
+    />
+  )
 }
 
-export default FlutterScreen;
+export default FlutterScreen
